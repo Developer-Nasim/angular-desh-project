612dd9035913b26218ea12c1a9dd6d4546df0e6a
Developer-Nasim <unique0coder@gmail.com>
2021-01-27 05:02:34 +0600

    ang-project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nasim'`, () => {
+  it(`should have as title 'desh-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nasim');
+    expect(app.title).toEqual('desh-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('nasim app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('desh-app app is running!');
   });
 });
